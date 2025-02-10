6688acb4d0ca14f5230166eada02e6ae99210f0e
Ranjith <ranjithkumarvkpm2@gmail.com>
2025-02-10 23:26:40 +0530

    Updated Work Details

--- src/UI/work-section/Timeline.jsx
+++ src/UI/work-section/Timeline.jsx
@@ -11,9 +11,12 @@ const TimelineData = [
     stuffIDid: [
       "Built responsive and dynamic user interfaces using React.js from Scratch, ensuring a seamless user experience across various devices and browsers.",
       "Implemented efficient state management solutions with Redux, optimizing data flow and improving application performance.",
-      "Created reusable components to enhance productivity and maintainability of the codebase.",
+      "Created reusable Custom components to enhance productivity and maintainability of the codebase.",
+      "Utilized Ant Design (antd) components to create modern and intuitive user interfaces.",      
       "Improved site speed by 20% through code splitting and lazy loading.",
       "Increased user engagement by 30% through intuitive design and improved navigation.",
+      "Collaborated closely with backend developers and designers to integrate APIs and create user-centric designs that met business requirements.",
+      "Participated in agile sprints, and daily stand-ups, ensuring high-quality deliverables and efficient collaboration."
     ],
   },
 ];
